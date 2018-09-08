ea50147720516f980ac7697407c586de291ff08d
Gogo <gogo81745.moe@gmail.com>
2018-09-08 21:43:48 +0800

    简化 DragonCurve 代码

--- week02/src/DragonCurve.java
+++ week02/src/DragonCurve.java
@@ -1,51 +1,38 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class DragonCurve {
 
-    private enum Turn {
-        L, R;
+    private static Map<String, String> reverseCharMap = new HashMap<>();
 
-        static {
-            L.reverse = R;
-            R.reverse = L;
-        }
-
-        Turn reverse;
-
-        public Turn reverse() {
-            return reverse;
-        }
+    static {
+        reverseCharMap.put("L", "R");
+        reverseCharMap.put("R", "L");
     }
 
-    private List<Turn> turns = new ArrayList<>();
+    private List<String> turns = new ArrayList<>();
 
-    private DragonCurve(List<Turn> previous) {
-        List<Turn> reverse = new ArrayList<>(previous);
+    private DragonCurve(List<String> previous) {
+        List<String> reverse = new ArrayList<>(previous);
         Collections.reverse(reverse);
         reverse = reverse.stream()
-                .map(Turn::reverse)
+                .map(reverseCharMap::get)
                 .collect(Collectors.toList());
 
         turns.addAll(previous);
-        turns.add(Turn.L);
+        turns.add("L");
         turns.addAll(reverse);
     }
 
     @Override
     public String toString() {
         return "F" + turns.stream()
-                .map(Object::toString)
                 .map(it -> it + "F")
                 .collect(Collectors.joining());
     }
 
     public static void main(String[] args) {
-
-        List<Turn> n0 = new ArrayList<>();
-        DragonCurve d1 = new DragonCurve(n0);
+        DragonCurve d1 = new DragonCurve(new ArrayList<>());
         DragonCurve d2 = new DragonCurve(d1.turns);
         DragonCurve d3 = new DragonCurve(d2.turns);
         DragonCurve d4 = new DragonCurve(d3.turns);
