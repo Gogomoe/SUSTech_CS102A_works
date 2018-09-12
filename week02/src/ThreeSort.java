1e31a9c5117c78145e9c95900e7613c0df15dd10
Gogo <gogo81745.moe@gmail.com>
2018-09-12 11:50:20 +0800

    update ThreeSort

--- week02/src/ThreeSort.java
+++ week02/src/ThreeSort.java
@@ -1,19 +1,19 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class ThreeSort {
 
     public static void main(String[] args) {
-        int[] ints = Arrays.stream(args)
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
 
-        Arrays.sort(ints);
-        List<String> strings = Arrays.stream(ints)
-                .mapToObj(Integer::toString)
-                .collect(Collectors.toList());
+        int max1 = Math.max(a, b);
+        int min1 = Math.min(a, b);
 
-        System.out.println(String.join(" ", strings));
+        int max = Math.max(max1, c);
+        int min2 = Math.min(max1, c);
+
+        int middle = Math.max(min1, min2);
+        int min = Math.min(min1, min2);
+
+        System.out.println(max + " " + middle + " " + min);
     }
 }
