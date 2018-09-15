69a7d6e0bb8545500f928373e0e7ec6741475ba9
Gogo <gogo81745.moe@gmail.com>
2018-09-16 07:52:47 +0800

    import exercises of class 3

---
+++ week03/src/ComputeAreaWithConsoleInput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ComputeAreaWithConsoleInput {
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter a radius
+        System.out.print("Enter a number for radius: ");
+        double radius = input.nextDouble();
+
+        // Compute area
+        double area = radius * radius * Math.PI;
+
+        // Display result
+        System.out.println("The area for the circle of radius " +
+                radius + " is " + area
+        );
+    }
+}
