e6b840dc425ca60c162793e6693394decaebefe7
Gogo <gogo81745.moe@gmail.com>
2018-10-07 12:53:06 +0800

    update Checksum, fix a bug

--- week04/src/Checksum.java
+++ week04/src/Checksum.java
@@ -2,20 +2,23 @@ public class Checksum {
 
     public static void main(String[] args) {
         int sum = 0;
+        StringBuilder check = new StringBuilder();
 
         for (int i = 0; i < 10; i++) {
             int digit = args[0].charAt(i) - '0';
 
             if (i % 2 == 0) {
                 sum += digit;
+                check.append(digit);
             } else {
                 sum += f(digit);
+                check.append(f(digit));
             }
         }
 
-        int checksum = (0 - sum % 10) % 10;
+        int checksum = (10 - sum % 10) % 10;
 
-        System.out.println(args[0] + checksum);
+        System.out.println(check.toString() + checksum);
     }
 
     private static int f(int i) {
