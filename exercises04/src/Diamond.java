fcae6462774410253b5d9159a408a34790597c35
Gogo <gogo81745.moe@gmail.com>
2018-10-09 14:09:45 +0800

    add lib assignment questions fix a bug in A1Q1

---
+++ exercises04/src/Diamond.java
@@ -0,0 +1,49 @@
+public class Diamond {
+
+    public static String diamond() {
+        return diamond(8);
+    }
+
+    public static String diamond(int n) {
+        return diamond(n, '*');
+
+    }
+
+    public static String diamond(int n, char flash) {
+        return diamond(n, flash, ' ');
+
+    }
+
+    public static String diamond(int n, char flash, char blank) {
+        StringBuilder sb = new StringBuilder();
+        int height = 2 * n - 1;
+        for (int y = 0; y < height; y++) {
+            int width = height - Math.abs(y - n + 1);
+            for (int x = 0; x < width; x++) {
+                if (y < n - 1) {
+                    if (x == n - 1 || x == width - 1 || x + y == n - 1) {
+                        sb.append(flash);
+                    } else {
+                        sb.append(blank);
+                    }
+                } else if (y == n - 1) {
+                    sb.append(flash);
+                } else {
+                    int start = y - n + 1;
+                    if (x >= start && (x != n - 1 || y == height - 1)) {
+                        sb.append(flash);
+                    } else {
+                        sb.append(blank);
+                    }
+                }
+
+            }
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(diamond(7, '*', '.'));
+    }
+}
--- lab1/src/A1Q1.java
+++ lab1/src/A1Q1.java
@@ -4,10 +4,10 @@ public class A1Q1 {
 
     public static void main(String[] args) {
         int r = Integer.parseInt(args[0]);
-        double p = r * 2 * PI;
         double s = r * r * PI;
-        System.out.printf("%.2f\n", p);
+        double p = r * 2 * PI;
         System.out.printf("%.2f\n", s);
+        System.out.printf("%.2f\n", p);
     }
 
 }
