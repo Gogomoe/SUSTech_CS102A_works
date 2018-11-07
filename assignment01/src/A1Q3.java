f50054ca4f24b899f17bebc91dfcff01b60ed62e
Gogo <gogo81745.moe@gmail.com>
2018-11-07 21:57:02 +0800

    Fix fake FP in ThreeSortByFP

--- assignment01/src/A1Q3.java
+++ assignment01/src/A1Q3.java
@@ -1,13 +1,13 @@
-import static java.lang.Math.pow;
+import static java.lang.Math.*;
 
 public class A1Q3 {
 
     public static void main(String[] args) {
         double l = Double.parseDouble(args[0]);
-        System.out.printf("%.2f\n", 0.433 * pow(l, 2));
-        System.out.printf("%.2f\n", 1.00 * pow(l, 2));
-        System.out.printf("%.2f\n", 1.72 * pow(l, 2));
-        System.out.printf("%.2f\n", 2.598 * pow(l, 2));
+        System.out.printf("%.2f\n", 3 / (4 * tan(toRadians(180.0 / 3))) * pow(l, 2));
+        System.out.printf("%.2f\n", 4 / (4 * tan(toRadians(180.0 / 4))) * pow(l, 2));
+        System.out.printf("%.2f\n", 5 / (4 * tan(toRadians(180.0 / 5))) * pow(l, 2));
+        System.out.printf("%.2f\n", 6 / (4 * tan(toRadians(180.0 / 6))) *pow(l, 2));
     }
 
 }
--- exercises01/src/ThreeSortByFP.java
+++ exercises01/src/ThreeSortByFP.java
@@ -1,19 +1,15 @@
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class ThreeSortByFP {
 
     public static void main(String[] args) {
-        int[] ints = Arrays.stream(args)
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        Arrays.sort(ints);
-        List<String> strings = Arrays.stream(ints)
-                .mapToObj(Integer::toString)
-                .collect(Collectors.toList());
-
-        System.out.println(String.join(" ", strings));
+        System.out.println(
+                Arrays.stream(args)
+                        .mapToInt(Integer::parseInt)
+                        .sorted()
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining(" "))
+        );
     }
 }
